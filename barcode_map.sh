422a42892879704d5cb78313841854fa487e144c
schmigle <124629898+schmigle@users.noreply.github.com>
2025-07-03 13:54:49 -0700

    Update barcode_map.sh

--- barcode_map.sh
+++ barcode_map.sh
@@ -320,7 +320,7 @@ done
 # Perform alignment
 minimap2 -ax map-ont -t "$threads" "$fasta" search_reads.fastq -o "$sam"
 # Process results
-print("Top matches")
+echo "Top matches"
 if [ ! -z "$annotation" ]; then
     process_mapping_results "$sam" "$annotation" "$matches"
 else
